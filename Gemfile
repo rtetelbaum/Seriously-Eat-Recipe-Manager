8ad6ac251438899bb2ffb9a7afaa77bacbecd8ea
rtetelbaum <rtetelbaum@mailbox.org>
2020-09-28 15:04:06 -0400

    environment setup complete

--- Gemfile
+++ Gemfile
@@ -5,3 +5,10 @@ gem "sinatra-activerecord"
 gem "sqlite3"
 gem "pry"
 gem "require_all"
+# everything required below added by project devs, everything above from project template
+gem 'rest-client'
+# gem 'activerecord'
+# gem 'rake'
+# gem 'tty-prompt'
+# gem 'tty-font'
+# gem 'awesome_print'
--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,20 @@
 require 'bundler'
 Bundler.require
 
-ActiveRecord::Base.establish_connection(adapter: 'sqlite3', database: 'db/development.db')
+ActiveRecord::Base.establish_connection(
+	adapter: 'sqlite3',
+	database: 'db/development.db'
+)
+# turns off SQL logger, added by project devs
+ruby ActiveRecord::Base.logger = nil
+# alternative: ActiveRecord::Base.logger = Logger.new(STDOUT)
+
 require_all 'lib'
+# everything required below added by project devs, everything above from project template
+require_all 'app'
+require 'pry'
+require 'json'
+require 'rest-client'
+# require 'tty-prompt'
+# require 'tty-font'
+# require 'awesome_print'
