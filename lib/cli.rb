4a86fa8b30fb474924bf3a2f48b0d4aaa9acfee6
miShelbyT <mi.shelbyrose@gmail.com>
2020-09-29 18:47:45 -0400

    made some minor edits to menu in cli

--- lib/cli.rb
+++ lib/cli.rb
@@ -13,13 +13,8 @@ class CLI
     def menu
         puts "Choose an option!"
         puts "1. Browse All Recipes"
-        puts "2. Save A Recipe"
-        puts "3. Heart A Recipe"
-        puts "4. Leave Notes To Self (Regarding a Recipe)"
-				puts "5. View Your Recipe Box"
-				puts "6. View Hearted Recipes"
-        puts "7. Remove A Recipe From Your Recipe Box"
-        puts "8. Exit"
+        puts "2. View Your Recipe Box"
+        puts "3. Exit" 
         choice = gets.chomp 
         
     end
