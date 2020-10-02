4d8c887f621e92ce32fedcefc32f763a0f21754d
rtetelbaum <rtetelbaum@mailbox.org>
2020-10-02 03:09:18 -0400

    testing with TTY prompt failed

--- lib/cli.rb
+++ lib/cli.rb
@@ -5,116 +5,99 @@ class CLI
 	attr_accessor :current_user, :pastel
 
 	$pastel = Pastel.new(eachline: "\n")
+	$prompt = TTY::Prompt.new
 
 	def welcome
 		font = TTY::Font.new(:standard)
 		starvin = Sound.new('./starvin.wav')
-		puts $pastel.bright_magenta.bold(font.write("Welcome  to"))
-		sleep(1)
-		puts $pastel.bright_magenta.bold(font.write("Seriously,  Eat!"))
-		sleep(1)
-		puts $pastel.bright_magenta.bold(font.write("Where  seriously..."))
-		sleep(3)
-		puts $pastel.bright_magenta.bold(font.write("we  want  you  to  eat."))
-		starvin.play
-		sleep(4)
+		# puts $pastel.bright_magenta.bold(font.write("Welcome  to"))
+		# sleep(1)
+		# puts $pastel.bright_magenta.bold(font.write("Seriously,  Eat!"))
+		# sleep(1)
+		# puts $pastel.bright_magenta.bold(font.write("Where  seriously..."))
+		# sleep(3)
+		# puts $pastel.bright_magenta.bold(font.write("we  want  you  to  eat."))
+		# starvin.play
+		# sleep(4)
 		puts ""
-		puts $pastel.bright_magenta.on_blue.bold("Do you have a 'Seriously, Eat!' account? (y/n)")
+		choice = $prompt.yes?($pastel.bright_magenta.on_blue.bold("Do you have a 'Seriously, Eat!' account?"))
 		puts ""
-		choice = gets.chomp
-		puts""
-		if choice == "y"
-			puts $pastel.bright_magenta.on_blue.bold("Please enter your username:")
-			puts ""
-			username = gets.chomp
-			puts""
+		if choice == true
+			username = $prompt.ask($pastel.bright_magenta.on_blue.bold("Please enter your username:"))
 			if User.find_by(user_name: username)
 				@current_user = User.find_by(user_name: username)
+				puts ""
 				self.options
 			else
-				puts $pastel.bright_magenta.on_blue.bold("This username does not exist. Would you like create an account? (y/n)")
-				puts ""
-				choice = gets.chomp
+				choice = $prompt.yes?($pastel.bright_magenta.on_blue.bold("This username does not exist. Would you like create an account?"))
 				puts ""
 				self.account_option(choice)
 			end
-		elsif choice == "n"
-			puts $pastel.bright_magenta.on_blue.bold("Would you like create an account? (y/n)")
-				puts ""
-				choice = gets.chomp
+		elsif choice == false
+			choice = $prompt.yes?($pastel.bright_magenta.on_blue.bold("Would you like create an account?"))
 				puts ""
 				self.account_option(choice)
 		end
 	end
 
 	def account_option(choice)
-		if choice == "y"
+		if choice == true
 			self.create_new_user
 			self.options
-		elsif choice == "n"
+		elsif choice == false
 			puts $pastel.bright_magenta.on_blue.bold("Goodbye!")
 			puts ""
 		end
 	end
 
 	def create_new_user
-		puts $pastel.bright_magenta.on_blue.bold("Please enter a new username:")
+		new_username = $prompt.ask($pastel.bright_magenta.on_blue.bold("Please enter a new username:"))
 		puts "" 
-		new_username = gets.chomp
-		puts ""
 		@current_user = User.create(user_name: new_username)
 		puts $pastel.bright_magenta.on_blue.bold("Welcome, #{new_username}. We hope you're hungry!")
 		puts ""
 	end
 
 	def options
-		puts $pastel.bright_magenta.on_blue.bold("OPTIONS MENU")
-		puts $pastel.bright_magenta("Enter an option number:")
-		puts $pastel.bright_magenta("1. Browse recipes")
-		puts $pastel.bright_magenta("2. View your Recipe Box (saved recipes)")
-		puts $pastel.bright_magenta("3. Exit")
+		choice = $prompt.select($pastel.bright_magenta.on_blue.bold("OPTIONS MENU")) do |menu|
+			menu.choice $pastel.bright_magenta("Browse recipes"), 1
+			menu.choice $pastel.bright_magenta("View your Recipe Box (saved recipes)"), 2
+			menu.choice $pastel.bright_magenta("Exit"), 3
+		end
 		puts ""
-		choice = gets.chomp
-			if choice == "1" || choice == "1."
-				puts ""
-				self.browse_recipes
-			elsif choice == "2" || choice == "2."
-				puts ""
-				self.view_recipe_box
-			elsif choice == "3" || choice == "3."
-				puts ""
-				puts $pastel.bright_magenta.on_blue.bold("Goodbye!")
-				puts ""
-				exit
-			end
+		if choice == 1
+			self.browse_recipes
+		elsif choice == 2
+			self.view_recipe_box
+		elsif choice == 3
+			puts $pastel.bright_magenta.on_blue.bold("Goodbye!")
+			puts ""
+			exit
+		end
 	end
 
 	def browse_recipes
-		puts $pastel.bright_magenta.on_blue.bold("BROWSE RECIPES")
-		puts $pastel.bright_magenta("How would you like to search? (choose number)")
-		puts $pastel.bright_magenta("1. Search by recipe name")
-		puts $pastel.bright_magenta("2. View popular recipes")
-		puts $pastel.bright_magenta("3. View healthy recipes")
-		puts $pastel.bright_magenta("4. View vegetarian recipes")
-		puts $pastel.bright_magenta("5. Return to Options Menu")
 		puts ""
-		choice = gets.chomp
-			if choice == "1" || choice == "1."
-				puts ""
-				self.search_by_name
-			elsif choice == "2" || choice == "2."
-				puts ""
-				self.search_by_popular
-			elsif choice == "3" || choice == "3."
-				puts ""
-				self.search_by_healthy
-			elsif choice == "4" || choice == "4."
-				puts ""
-				self.search_by_vegetarian
-			elsif choice == "5" || choice == "5."
-				puts ""
-				self.options
-			end
+		puts $pastel.bright_magenta.on_blue.bold("BROWSE RECIPES")
+		choice = $prompt.select($pastel.bright_magenta("How would you like to search?")) do |menu|
+		menu.choice $pastel.bright_magenta("Search by recipe name"), 1
+		menu.choice $pastel.bright_magenta("View popular recipes"), 2
+		menu.choice $pastel.bright_magenta("View healthy recipes"), 3
+		menu.choice $pastel.bright_magenta("View vegetarian recipes"), 4
+		menu.choice $pastel.bright_magenta("Return to Options Menu"), 5
+		end
+		puts ""
+		if choice == 1
+			self.search_by_name
+		elsif choice == 2
+			self.search_by_popular
+		elsif choice == 3
+			self.search_by_healthy
+		elsif choice == 4
+			self.search_by_vegetarian
+		elsif choice == 5
+			self.options
+		end
 	end
 
 	def recipe_options(recipe_title)
@@ -122,37 +105,32 @@ class CLI
 		puts ""
 		puts $pastel.bright_magenta("The recipe you've selected is: '#{recipe_title}'")
 		puts ""
-		puts $pastel.bright_magenta("Enter an option number:")
-		puts $pastel.bright_magenta("1. View website for this recipe")
-		puts $pastel.bright_magenta("2. Save recipe to Recipe Box")
-		puts $pastel.bright_magenta("3. Go back to Browse Recipes")
+		choice = $prompt.select($pastel.bright_magenta("Enter an option number:")) do |menu|
+		menu.choice $pastel.bright_magenta("View website for this recipe"), 1
+		menu.choice $pastel.bright_magenta("Save recipe to Recipe Box"), 2
+		menu.choice $pastel.bright_magenta("Go back to Browse Recipes"), 3
+		end
 		puts ""
-		choice = gets.chomp
-		puts ""
-		if choice == "1" || choice == "1."
+		if choice == 1
 			self.load_recipe_url(recipe_title)
 			self.recipe_options(recipe_title)
-		elsif choice == "2" || choice == "2."
+		elsif choice == 2
 			current_recipe = Recipe.find_by(title: recipe_title)
 			RecipeBox.create(user_id: @current_user.id, recipe_id: current_recipe.id)
 			puts $pastel.bright_magenta("Your recipe '#{recipe_title}' has been savedto your Recipe Box.")
 			self.recipe_options(recipe_title)
-		elsif choice == "3" || choice == "3."
+		elsif choice == 3
 			self.browse_recipes
 		end
 	end
 
 	def search_by_name
 		puts $pastel.bright_magenta.on_blue.bold("SEARCH RECIPES BY NAME")
-		puts $pastel.bright_magenta("Please enter a search term:")
-		puts ""
-		search_term = gets.chomp
+		search_term = $prompt.ask($pastel.bright_magenta("Please enter a search term:"))
 		puts ""
 		results = Recipe.where("title LIKE ?", "%" + search_term + "%").pluck(:title)
 			until !results.empty?
-				puts $pastel.bright_magenta("No matching results, please try again:")
-				puts ""
-				search_term = gets.chomp
+				search_term = $prompt.ask($pastel.bright_magenta("No matching results, please try again:"))
 				puts ""
 				results = Recipe.where("title LIKE ?", "%" + search_term + "%").pluck(:title)
 				puts ""
@@ -179,66 +157,55 @@ class CLI
 	end
 
 	def recipe_results(results)
-		puts $pastel.bright_magenta("Results:")
-		results.each_with_index { |r, i| puts $pastel.bright_magenta("#{i.next}. #{r}") }
+		recipe_title = $prompt.select($pastel.bright_magenta("Select a recipe:")) do |menu|
+			results.each { |r| menu.choice $pastel.bright_magenta("#{r}") }
+			menu.choice $pastel.bright_magenta("Go back to Browse Recipes"), -> { self.browse_recipes }
+		end
 		puts ""
-		puts $pastel.bright_magenta("Please enter a recipe number or type 'exit' to return to Browse Recipes:")
-		puts ""        
-		choice = gets.chomp
-		puts ""
-		self.browse_recipes if choice == "exit"
-		index = choice.to_i - 1
-		recipe_title = results[index]
 		self.recipe_options(recipe_title)
 	end
 
 	def view_recipe_box
 		puts $pastel.bright_magenta.on_blue.bold("YOUR RECIPE BOX")
 		results = RecipeBox.where(user_id: @current_user.id)
-			if results.empty?
-				puts $pastel.bright_magenta("Your Recipe Box is empty.")
-				puts ""
-				self.options
-			elsif !results.empty?
-				results.each_with_index { |r, i| puts $pastel.bright_magenta("#{i.next}. #{Recipe.find(r.recipe_id).title}")}
-				puts ""
-				puts $pastel.bright_magenta("Please enter a recipe number to see it, view/add a note to it, or delete it from your Recipe Box:")
-				puts $pastel.bright_magenta("(Or type 'exit' to return to the Options Menu)")
-				puts ""
-				choice = gets.chomp
-				puts ""
-				self.options if choice == "exit"
-				index = choice.to_i - 1
-				recipe_title = Recipe.find(results[index].recipe_id).title
-				modify_recipe_box(recipe_title)
+		if results.empty?
+			puts $pastel.bright_magenta("Your Recipe Box is empty.")
+			puts ""
+			self.options
+		elsif !results.empty?
+			recipe_title = $prompt.select($pastel.bright_magenta("Select a recipe to see it, view/add a note to it, or delete it from your Recipe Box:")) do |menu|
+				results.each { |r| menu.choice $pastel.bright_magenta("#{Recipe.find(r.recipe_id).title}") }
+				menu.choice $pastel.bright_magenta("Go back to Browse Recipes"), -> { self.options }
 			end
+			puts ""
+			modify_recipe_box(recipe_title)
+		end
 	end
 
 	def modify_recipe_box(recipe_title)
 		puts $pastel.bright_magenta.on_blue.bold("SAVED RECIPE OPTIONS")
 		puts $pastel.bright_magenta("The saved recipe you've selected is: '#{recipe_title}'")
-		puts $pastel.bright_magenta("Enter an option number:")
-		puts $pastel.bright_magenta("1. Open website for this recipe")
-		puts $pastel.bright_magenta("2. View your note for this recipe")
-		puts $pastel.bright_magenta("3. Add/update note for this recipe")
-		puts $pastel.bright_magenta("4. Remove note from this recipe")
-		puts $pastel.bright_magenta("5. Delete this recipe from your Recipe Box")
-		puts $pastel.bright_magenta("6. Go back to your Recipe Box")
+		choice = $prompt.select($pastel.bright_magenta("Enter an option number:")) do |menu|
+			menu.choice $pastel.bright_magenta("Open website for this recipe"), 1
+			menu.choice $pastel.bright_magenta("View your note for this recipe"), 2
+			menu.choice $pastel.bright_magenta("Add/update note for this recipe"), 3
+			menu.choice $pastel.bright_magenta("Remove note from this recipe"), 4
+			menu.choice $pastel.bright_magenta("Delete this recipe from your Recipe Box"), 5
+			menu.choice $pastel.bright_magenta("Go back to your Recipe Box"), 6
+		end
 		puts ""
-		choice = gets.chomp
-		puts ""
-			if choice == "1" || choice == "1."
+			if choice == 1
 				self.load_recipe_url(recipe_title)
 				self.modify_recipe_box(recipe_title)
-			elsif choice == "2" || choice == "2."
+			elsif choice == 2
 				self.view_recipe_note(recipe_title)
-			elsif choice == "3" || choice == "3."
+			elsif choice == 3
 				self.add_note_to_recipe(recipe_title)
-			elsif choice == "4" || choice == "4."
+			elsif choice == 4
 				self.remove_note_from_recipe(recipe_title)
-			elsif choice == "5" || choice == "5."
+			elsif choice == 5
 				self.delete_recipe(recipe_title)
-			elsif choice == "6" || choice == "6."
+			elsif choice == 6
 				self.view_recipe_box
 			end
 	end
